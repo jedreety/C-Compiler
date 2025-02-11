cfafb75bd3ee976566903f1d261cb02fc9a5111d
jedreety <130364840+jedreety@users.noreply.github.com>
2025-02-11 11:12:23 +0100

    Remove the AsmToExeTest folder
    Removed because it no longer served any purpus

--- AsmToExeTest/test.asm
+++
@@ -1,8 +0,0 @@
-section .text
-global main
-extern ExitProcess   ; Declare the Win32 API function
-
-main:
-    mov rcx, 0       ; Exit code 0 (passed via RCX)
-
-call ExitProcess ; Call Windows API
